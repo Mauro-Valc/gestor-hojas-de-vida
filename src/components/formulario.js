7f729019c225eb35521d1714cb7868e23ce30661
Mauricio Valencia Cosme <mauricio.valencia@pragma.com.co>
2020-04-29 19:24:57 -0500

    Prueba componentes

---
+++ src/components/formulario.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navigation() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="text-white">
+
+            </a>
+        </nav>
+      );
+}
+
+export default Navigation;
